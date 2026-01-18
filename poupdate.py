e0bfed0940b842ea15fb2293e0a87b32d009daa2
Vasily Zakharov <v.zakharov@wwpass.com>
2026-01-19 01:47:36 +0300

    gettext/ru.po sorted

--- poupdate.py
+++ poupdate.py
@@ -6,7 +6,7 @@ from re import escape, search
 from itertools import chain
 from typing import Final
 
-from polib import pofile, POEntry
+from polib import pofile, POFile
 
 from beartype import beartype as typechecked
 
@@ -34,18 +34,24 @@ def getOccurrences(fileName: str, msgid: str) -> Iterator[Occurrence]:
 @typechecked
 def main() -> None:
     poFileName = './gettext/ru.po'
-    po = pofile(poFileName, wrapwidth = 200, encoding = 'utf-8', check_for_duplicates = True)
-    entries: list[POEntry] = []
+    po = pofile(poFileName, wrapwidth = 999, encoding = 'utf-8', check_for_duplicates = True)
 
     for entry in po:
         fileNames = tuple(sorted({occurrence[0] for occurrence in entry.occurrences}))
         if occurrences := list(chain.from_iterable(getOccurrences(fileName, entry.msgid) for fileName in fileNames if fileName != KEEP)):
             entry.occurrences = occurrences
-            entries.append(entry)
         elif KEEP not in fileNames:
             po.remove(entry)
-    po.save(poFileName)
-    po.save_as_mofile(poFileName.replace('.po', '.mo'))
+
+    newPO = POFile(wrapwidth = 999, encoding = 'utf-8', check_for_duplicates = True)
+    newPO.header = po.header
+    newPO.metadata = po.metadata
+
+    for entry in sorted(po, key = lambda entry: (entry.occurrences[0][0], int(entry.occurrences[0][1]))):
+        newPO.append(entry)
+
+    newPO.save(poFileName)
+    newPO.save_as_mofile(poFileName.replace('.po', '.mo'))
 
 if __name__ == '__main__':
     main()
